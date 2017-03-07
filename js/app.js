f7191eafbe46c7e627fd2f3757210f1a3ed81827
Relvin Gonzalez <aerostrings@gmail.com>
2017-03-07 18:23:33 -0400

    First new commit

--- js/app.js
+++ js/app.js
@@ -1,21 +1,23 @@
-var app = angular.module('Turista',['leaflet-directive','ngRoute']);
-app.config(function($routeProvider){
-	$routeProvider
-  	.when('/',{
-  		controller: 'MainController',
-    	templateUrl: 'views/main.html',
-			resolve: {
-				position: function (myLocation) {
-					return myLocation;
-				}
-			}
-  	})
-  	.when('/about',{
-  		controller: 'AboutController',
-    	templateUrl: 'views/about.html'
-  	})
-  	.otherwise({
-  		redirectTo: '/'
-  });
+angular.module('Turista',['leaflet-directive','ngRoute'])
+	.config(config);
 
-});
+function config($routeProvider){
+    $routeProvider
+        .when('/',{
+            controller: 'MainController',
+            templateUrl: 'views/main.html',
+            resolve: {
+                position: function (myLocation) {
+                    return myLocation;
+                }
+            }
+        })
+        .when('/about',{
+            controller: 'AboutController',
+            templateUrl: 'views/about.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+}
